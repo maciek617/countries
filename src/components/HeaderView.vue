a75fd00b55daefe19f2d9d6ebebe4ff04568d86c
Maciej Gosztyła <r3cenzialny@gmail.com>
2022-05-04 20:07:07 +0200

    Initial commit

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1>Where in the world?</h1>
+    <div class="toggle_mode">
+
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderView"
+}
+</script>
+
+<style scoped>
+
+</style>
